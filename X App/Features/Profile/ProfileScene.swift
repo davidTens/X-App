ffa5f29e54d92998355fe926f1c75e94e225adbc
david <tensdavid@gmail.com>
2023-07-19 17:19:11 +0400

    ProfileScene init, ChatScene body

--- X App/Features/Profile/ProfileScene.swift
+++ X App/Features/Profile/ProfileScene.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 
-struct ChatScene: View {
+struct ProfileFeed: View {
 
     var body: some View {
         ZStack {
             Color.accentColor
                 .ignoresSafeArea()
             VStack {
-                Text("Chat")
+                Text("Profile")
                     .font(.init(.custom("Avenir Next", size: 50)))
                     .padding(.init(top: 130, leading: 50, bottom: 0, trailing: 0))
                     .frame(maxWidth: .infinity, alignment: .leading)
