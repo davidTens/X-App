466da14ec0ba1ef3e357f9e1d7c581969f8115ad
david <tensdavid@gmail.com>
2023-07-19 15:33:13 +0400

    [TabViewModel] - comments regarding coordinator

--- X App/Features/Tab/TabViewModel.swift
+++ X App/Features/Tab/TabViewModel.swift
@@ -33,6 +33,10 @@ final class TabViewModel: ObservableObject {
     private func setupBindings() {
         didSingleTap
             .sink(receiveValue:  { item in
+                // the event must be sent to coordinator
+                // coordinator should be responsible to draw corresponding view
+                // in the tab coordinator make sure the object is subscribed to *didSingleTap* ^^
+                // so it will be able to recieve events and act accordingly
                 Logger().debug(
                     "::[TabViewModel] \(item.rawValue) tapped"
                 )
