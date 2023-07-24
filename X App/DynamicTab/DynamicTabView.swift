41c38fb972981e2f4999829580000265bf490ed9
david <tensdavid@gmail.com>
2023-07-24 18:28:36 +0400

    color update

--- X App/DynamicTab/DynamicTabView.swift
+++ X App/DynamicTab/DynamicTabView.swift
@@ -14,6 +14,8 @@ struct DynamicTabView: View {
 
     var body: some View {
         VStack {
+            Color.accentColor
+                .ignoresSafeArea()
             HStack {
                 Spacer()
                 tabView()
