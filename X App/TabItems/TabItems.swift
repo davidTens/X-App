4dd8742a9d710cf0ad058da4f4568e17d5537ddd
david <tensdavid@gmail.com>
2023-07-26 13:10:52 +0400

    new custom tab implementation

--- X App/TabItems/TabItems.swift
+++ X App/TabItems/TabItems.swift
@@ -1,8 +1,14 @@
 import Foundation
+import SwiftUI
 
-enum TabItems: String, CaseIterable {
+enum TabItems: String, CaseIterable, Identifiable, IconsProviding {
+    var id: Self { return self }
     case home
     case feed
     case chat
     case profile
+
+    var icon: Image {
+        Image(self.rawValue)
+    }
 }
